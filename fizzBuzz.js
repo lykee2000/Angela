2fe6377c02b0a30b5e678a38a3069a06f000df6a
yoongyo <lykee2000@naver.com>
2021-01-09 21:13:19 +0900

    fizzBuzz.js

---
+++ fizzBuzz.js
@@ -0,0 +1,18 @@
+var output = [];
+var cnt = 1;
+function fizzBuzz(){
+  if (cnt % 3 === 0 & cnt % 5 === 0){
+    output.push("FizzBuzz");
+  }
+  else if (cnt % 3 === 0){
+    output.push("Fizz");
+  }
+  else if (cnt % 5 === 0){
+    output.push("Buzz");
+  }
+  else{
+    output.push(cnt);
+  }
+  cnt++;
+  console.log(output);
+}
